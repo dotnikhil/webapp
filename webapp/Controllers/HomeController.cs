73437766c6ab609ec942f7cdffc26096e8c60fe9
Nikhil Maini <Nikhil Maini@MANISHA>
2025-07-02 13:09:21 +0530

    changes

--- webapp/Controllers/HomeController.cs
+++ webapp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace webapp.Controllers
             return View();
         }
 
+        public IActionResult NewPage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
